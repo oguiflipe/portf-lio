c080059d5d512be478583795932c9cab2ee9aedb
oguiflipe <guilhermeflipe@outlook.com>
2024-01-15 21:59:55 -0300

    Atualização do .Json e consumo dos dados.

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/RenanJPaula/js-developer-portfolio/main/data/profile.json';
+    const url = 'https://raw.githubusercontent.com/oguiflipe/portf-lio/main/data/profile.json?token=GHSAT0AAAAAACMXP7EYRKCZTUL56LFQH2D6ZNF2IDQ';
     const fetching = await fetch(url)
 
     return await fetching.json()
