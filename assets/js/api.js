0a7e9b7120c3902eb6337c741741887ec81e1161
oguiflipe <guilhermeflipe@outlook.com>
2024-01-18 15:51:23 -0300

    Alteração no caminho assets

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/oguiflipe/portf-lio/main/data/profile.json?token=GHSAT0AAAAAACMXP7EZ5DLZ5XWLU5GU5WESZNJMNIA';
+    const url = 'https://raw.githubusercontent.com/oguiflipe/portf-lio/main/data/profile.json?token=GHSAT0AAAAAACMXP7EZK3IA5GEW42YFLZY4ZNJOAOA';
     const fetching = await fetch(url)
 
     return await fetching.json()
--- assets/js/main.js
+++ assets/js/main.js
@@ -36,11 +36,44 @@ function updateHardSkills(profileData){
    hardSkills.innerHTML = profileData.skills.hardSkills.map(skill => `<li><img src="${skill.logo}" alt="${skill.name}" title="${skill.name}"/></li>`).join('');
 }
 
+function updateLanguages(profileData){
+    const languages = document.getElementById('languages');
+    languages.innerHTML = profileData.languages.map(lang => `<li>${lang}</li>`).join('');
+ }
 
 
+ function updateLanguages(profileData){
+    const languages = document.getElementById('languages');
+    languages.innerHTML = profileData.languages.map(lang => `<li>${lang}</li>`).join('');
+ }
+
+ function updatePortfolio(profileData){
+    const portfolio = document.getElementById('portfolio');
+    portfolio.innerHTML = profileData.portfolio.map(port => `<li>
+    <h3 class="title github">${port.name}</h3>
+    <a href="${port.url}" target="_blank">${port.url}</a>
+  </li>`).join('');
+ }
+
+ function updateProfessionalExperience(profileData){
+    const professionalExperience = document.getElementById('professionalExperience')
+    professionalExperience.innerHTML = profileData.professionalExperience.map(experience => `
+    <li>
+        <h3 class="title">${experience.name}</h3>
+        <p class="period">${experience.period}</p>
+        <p>${experience.description}</p>
+    </li>
+    `).join('')
+ }
+
+
+ //Chamando a função assincrona para mostrar na página.
 ( async  () => {
     const profileData = await fetchProfileData()
     updateProfileInfo(profileData)
     updateSoftSkills(profileData);
     updateHardSkills(profileData);
+    updateLanguages(profileData);
+    updatePortfolio(profileData);
+    updateProfessionalExperience(profileData);
 })();
