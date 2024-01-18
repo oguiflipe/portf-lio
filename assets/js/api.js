de51e0014c9795637a56641569354d7b581ae4b4
oguiflipe <guilhermeflipe@outlook.com>
2024-01-18 13:49:43 -0300

    Adicionando imagens de tecnologia

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,5 +1,5 @@
 async function fetchProfileData(){
-    const url = 'https://raw.githubusercontent.com/oguiflipe/portf-lio/main/data/profile.json?token=GHSAT0AAAAAACMXP7EYRKCZTUL56LFQH2D6ZNF2IDQ';
+    const url = 'https://raw.githubusercontent.com/oguiflipe/portf-lio/main/data/profile.json?token=GHSAT0AAAAAACMXP7EZ3CBA5LBPZEBV2C4AZNJKSBQ';
     const fetching = await fetch(url)
 
     return await fetching.json()
